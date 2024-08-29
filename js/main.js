5c9f3a6a2fbfb7efd5dff8754f5b4fd4a6258b3e
n2jovano <n2jovano@ryerson.ca>
2024-08-29 10:48:20 -0400

    Update title and test
    confirmed test that repository name change in github changes in github desktop

--- js/main.js
+++ js/main.js
@@ -1,45 +1,65 @@
-let plus1 = 1;
-let plus2 = 2;
-let plus3 = 3;
-let homeScore = 0;
-let guestScore = 0;
-let homeScoreResult = document.getElementById("home-score-result");
-let guestScoreResult = document.getElementById("guest-score-result");
+//document.getElementById("count-el").innerText = 6
 
-function homePlusOne() {
-    homeScore += plus1;
-    homeScoreResult.innerText = homeScore;
-    console.log(homeScore);
+let count = 0;
+let newCount =0;
+let mySave = 0;
+let list = "";
+let lion = "I\'d like to see the \"Lion King\".   \\";
+console.log(lion)
+let myWelcome = "Welcome guys!"
+let mySave2 = "";
+
+
+function increment() {
+    count += 1;
+    console.log(count);
+    let insideFunction = 3;  //only lives inside scope of function
+    document.getElementById("count-el").innerText = count          // document.getElementById("count-el")    =>   <h2 id="count-el">0</h2>      innerText => 0
+}
+//  increment();
+//---------------------------------------------------------------
+
+// function myCounter() {
+//     count = count + 1;
+//     document.getElementById("count-el2").innerText = count;
+//     console.log(count)
+// }
+
+// below 2 functions old
+// function myListFunction() {
+//     list = document.getElementById("count-el").innerText;
+//     // console.log(list);
+//     list = document.getElementById("count-el2").innerText + "-" + list;
+//     document.getElementById("count-el2").innerText = list;
+//     // console.log(list);
+// }
+
+// function mySaveFunction() {
+//     mySave = document.getElementById("count-el").innerText;
+//     document.getElementById("save-el").innerText = mySave;
+//     myListFunction() 
+// }
+
+function mySaveFunction() {
+    mySave2 = document.getElementById("count-el").innerText;
+    // console.log("test here " + mySave2);
+    document.getElementById("save-el").innerText = mySave2;
+    list = document.getElementById("count-el").textContent + " - " + list;
+    document.getElementById("count-el2").innerText = list;
+    count = 0;
+    document.getElementById("count-el").textContent = count;
+
+    // console.log("test here: " + list);
 }
 
-function homePlusTwo() {
-    homeScore += plus2;
-    homeScoreResult.innerText = homeScore;
-    console.log(homeScore);
+
+
+
+
+function myWelcomeFunction() {
+    document.getElementById("welcome-el").innerText = myWelcome;
 }
 
-function homePlusThree() {
-    homeScore += plus3;
-    homeScoreResult.innerText = homeScore;
-    console.log(homeScore);
-}
+myWelcomeFunction();
 
-function guestPlusOne() {
-    guestScore += plus1;
-    guestScoreResult.innerText = guestScore;
-}
-
-function guestPlusTwo() {
-    guestScore += plus2;
-    guestScoreResult.innerText = guestScore;
-}
-
-function guestPlusThree() {
-    guestScore += plus3;
-    guestScoreResult.innerText = guestScore;
-}
-
-function newGame() {
-    guestScoreResult.innerText = 0;
-    homeScoreResult.innerText = 0;
-}
+//---------------
